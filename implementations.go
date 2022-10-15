1302ee138c7a5caccd433098f7bb10934da4c4d1
dominickbrasileiro <brasileiro.dominick@gmail.com>
2022-10-14 19:48:51 -0700

    feat: add ipinfo.io datasource

--- implementations.go
+++ implementations.go
@@ -28,3 +28,27 @@ func (d *IpifyDatasource) FetchIPv4() (string, bool) {
 
 	return string(bodyBytes), true
 }
+
+type IpInfoDatasource struct{}
+
+var _ IPv4Datasource = (*IpInfoDatasource)(nil)
+
+func NewIpInfoDatasource() *IpInfoDatasource {
+	return &IpInfoDatasource{}
+}
+
+func (d *IpInfoDatasource) FetchIPv4() (string, bool) {
+	res, err := http.Get("https://ipinfo.io/ip")
+
+	if err != nil {
+		return "", false
+	}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return "", false
+	}
+
+	return string(bodyBytes), true
+}
