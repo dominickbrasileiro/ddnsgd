36306cbac5f7fb0334595b89c9a31bef8c7b2503
dominickbrasileiro <brasileiro.dominick@gmail.com>
2022-10-14 19:49:11 -0700

    feat: add ifconfig.me datasource

--- implementations.go
+++ implementations.go
@@ -52,3 +52,27 @@ func (d *IpInfoDatasource) FetchIPv4() (string, bool) {
 
 	return string(bodyBytes), true
 }
+
+type IfConfigMeDatasource struct{}
+
+var _ IPv4Datasource = (*IfConfigMeDatasource)(nil)
+
+func NewIfConfigMeDatasource() *IfConfigMeDatasource {
+	return &IfConfigMeDatasource{}
+}
+
+func (d *IfConfigMeDatasource) FetchIPv4() (string, bool) {
+	res, err := http.Get("https://ifconfig.me")
+
+	if err != nil {
+		return "", false
+	}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return "", false
+	}
+
+	return string(bodyBytes), true
+}
